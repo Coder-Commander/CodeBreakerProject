9044ffe3fb39caf7b656a7577751549d648835e2
Coder-Commander <35503972+Coder-Commander@users.noreply.github.com>
2018-01-16 14:56:10 -0500

    step 7, 8, 9

--- src/assets/main.js
+++ src/assets/main.js
@@ -4,17 +4,24 @@ let attempt = document.getElementById('attempt');
 function guess() {
     let input = document.getElementById('user-guess');
     //add functionality to guess function here
+    if(answer == '' && attempt == ''){
+      setHiddenFields();
+    }
 }
 //implement new functions here
 
 function setHiddenFields() {
 
   answer.value = Math.floor(Math.random() * 10000);
+  attempt.value = 0;
 
   while(answer.length < 4)
     {
-      answer.value = '0' + answer.value.toString(); 
+      answer.value = '0' + answer.value.toString();
     }
+}
 
-
+function setMessage(text) {
+  let message = document.getElementById('message');
+  message.innerHTML = text;
 }
